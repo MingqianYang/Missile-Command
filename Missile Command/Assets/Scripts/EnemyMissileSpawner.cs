15f3fe998056e6bb6fe64f5e1b0ac0135b12564a
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-18 00:46:23 +1000

    Spawning Enemy Missiles

---
+++ Missile Command/Assets/Scripts/EnemyMissileSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissileSpawner : MonoBehaviour
+{
+    [SerializeField] private  GameObject missilePrefab;
+    [SerializeField] private float yPadding = 0.5f;
+    private float minX, maX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minX = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).x; 
+        maX = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0)).x;
+
+        float xValue = Random.Range(minX, maX);
+        float yValue  = minX = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+        Instantiate(missilePrefab, new Vector3(xValue, yValue + yPadding, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
