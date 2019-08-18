006bec82bfe954df9fa845f2954ce9b0b52aa8cc
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-18 12:11:31 +1000

    Missile Collision with cities

---
+++ Missile Command/Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private GameObject explosionPrefab;
+
+    private GameObject[] defenders;
+
+    private float m_Angle;
+
+    // The transform of defenders
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defenders = GameObject.FindGameObjectsWithTag("Defenders");
+        target = defenders[Random.Range(0, defenders.Length)].transform;
+
+        //transform.rotation = Quaternion.AngleAxis(180, Vector3.back);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+
+        // Missile always toward the target
+        Vector2 direction = target.position - transform.position;
+         m_Angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(m_Angle - 90, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Defenders"))
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnGUI()
+    {
+        //Output the angle found above
+       // GUI.Label(new Rect(25, 25, 200, 40), "Angle Between Objects" + m_Angle);
+    }
+}
