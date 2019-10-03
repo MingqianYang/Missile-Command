b7f25ae0ac0f9934a80554008373a7d0244fc935
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-10-03 16:37:36 +1000

    add leapmotion core asset

---
+++ leapmotion3d/Assets/LeapMotion/Modules/InteractionEngine/Examples/1. Interaction Objects/test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using LeapInternal;
+using Leap.Unity;
+public class test : MonoBehaviour
+{
+    Controller controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = new Controller();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // wait until Controller.isConnected() evaluates to true
+        //...
+
+        Frame frame = controller.Frame();
+
+       
+
+    }
+
+    public void GraspBegin()
+    {
+        Debug.Log("begin");
+    }
+
+    public void GraspEnd()
+    {
+        Debug.Log("end");
+    }
+}
